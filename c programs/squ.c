03c38d8774f431609287d7425b02f8bee7a0acf1
PallaviRohakale3103 <rohokalepallvi@gmail.com>
2025-05-31 14:05:50 +0530

    Add files via upload

---
+++ c programs/squ.c
@@ -0,0 +1,17 @@
+void main() 
+{
+    double number, square, cube;
+
+    
+    printf("Enter a number: ");
+    scanf("%lf", &number);
+
+    
+    square = number * number;
+    cube = number * number * number;
+    
+    printf("Square of %.2lf = %.2lf\n", number, square);
+    printf("Cube of %.2lf = %.2lf\n", number, cube);
+
+
+}
